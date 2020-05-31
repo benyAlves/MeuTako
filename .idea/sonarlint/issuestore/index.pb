92bc448c6eb3f5b3cb366a2fa32e71ca4267abba
Bernardo Maluleque <b.a.maluleque@gmail.com>
2020-06-01 01:33:12 +0200

    Initial Commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+q
+Aapp/src/main/java/com/udacity/maluleque/meutako/MainActivity.java,e\6\e6c89d73a543283559cb7de3ca3827edf9e8a77c
+r
+Bapp/src/main/java/com/udacity/maluleque/meutako/IntroActivity.java,3\f\3fbe192d4c1774cc8d7c15d632435a70824ed4b8
---
+++ app/src/main/java/com/udacity/maluleque/meutako/preferences/PreferencesManager.java
@@ -0,0 +1,35 @@
+package com.udacity.maluleque.meutako.preferences;
+
+import android.content.SharedPreferences;
+
+public class PreferencesManager {
+
+    private static PreferencesManager INSTANCE = null;
+    static SharedPreferences pref;
+    static SharedPreferences.Editor editor;
+
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PreferencesManager(SharedPreferences prefs) {
+        pref = prefs;
+        editor = pref.edit();
+    }
+
+    public static synchronized PreferencesManager getInstance(SharedPreferences prefs){
+        if (INSTANCE == null){
+            INSTANCE = new PreferencesManager(prefs);
+        }
+        return INSTANCE;
+    }
+
+    public void setFirstLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+    }
+
+    public boolean isFirstLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+
+}
