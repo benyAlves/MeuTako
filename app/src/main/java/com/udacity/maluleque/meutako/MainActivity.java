6476f5b159bd81cea62e44f8b329c024b0c48397
benyAlves <malulequeb@gmail.com>
2020-06-04 12:47:14 +0200

    fix: enable firebase database offline capabilities

--- app/src/main/java/com/udacity/maluleque/meutako/MainActivity.java
+++ app/src/main/java/com/udacity/maluleque/meutako/MainActivity.java
@@ -13,6 +13,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.FirebaseDatabase;
 import com.udacity.maluleque.meutako.adapters.FragmentAdapter;
 import com.udacity.maluleque.meutako.utils.DateUtils;
 import com.udacity.maluleque.meutako.utils.NetworkNotifier;
@@ -60,6 +61,7 @@ public class MainActivity extends AppCompatActivity implements TransactionListFr
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
         tabLayout.setupWithViewPager(viewPager);
 
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
     }
 
